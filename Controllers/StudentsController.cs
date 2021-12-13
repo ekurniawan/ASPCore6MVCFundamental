1e250eed4d7f86f15f079ff17d61932c2734c22b
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-13 11:40:35 +0700

    menambahkan student controller

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SampleASP.Models;
+
+namespace SampleASP.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ILogger<StudentsController> _logger;
+
+        public StudentsController(ILogger<StudentsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var student = new Student{
+                StudentID = 1,
+                FirstName = "Erick",
+                LastName = "Kurniawan",
+                EnrollmentDate = DateTime.Now
+            };
+
+            return View(student);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
