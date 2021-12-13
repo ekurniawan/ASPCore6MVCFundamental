605e80d3662f88234fa9cc6bb9fee0672c5c97c6
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-13 11:22:16 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SampleASP.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Student.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleASP.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+    
+}
