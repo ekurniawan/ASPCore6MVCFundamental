07e846e26572bde2ddd710db2d8d4746fd822bf0
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-12-14 15:10:34 +0700

    menambahkan IEnrollment

---
+++ DAL/IEnrollmentDAL.cs
@@ -0,0 +1,14 @@
+﻿using SampleASP.Models;
+
+namespace SampleASP.DAL
+{
+    public interface IEnrollmentDAL
+    {
+        IEnumerable<ViewEnrollment> GetAll();
+        IEnumerable<ViewEnrollment> GetByStudent(string studentId);
+        IEnumerable<ViewEnrollment> GetByCourse(string courseId);
+        void AddEnrollment(Enrollment enrollment);
+        void UpdateEnrollment(Enrollment enrollment);
+        void DeleteEnrollment(int id);
+    }
+}
